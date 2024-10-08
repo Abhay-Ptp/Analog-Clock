027347658f50f78443956f14e75e65ac14579b54
Abhay Pratap <165368044+Abhay-Ptp@users.noreply.github.com>
2024-10-08 17:46:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,70 @@
-let hour=document.getElementById('hour');
-let min=document.getElementById('min');
-let sec=document.getElementById('sec');
+let hour=document.getElementById('hour')
+let min=document.getElementById('min')
+let sec=document.getElementById('sec')
+
+let timeEl = document.querySelector(".time");
+let dateEl = document.querySelector(".date");
+
+
+
+const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec"
+  ];
+
+
 
 function setDial(){
-    let date= new Date()
-    let hh=date.getHours();
-    let mm=date.getMinutes();
-    let ss=date.getSeconds();
+    let time= new Date()
+    let hh=time.getHours();
+    let mm=time.getMinutes();
+    let ss=time.getSeconds();
 
-    // methods of rotation
+    let month = time.getMonth();
+    let day = time.getDay();
+    let date = time.getDate();
+
+    // methods of location----------------------
     let hRotate=30*hh + 0.5*mm;
     let mRotate=6*mm;
     let sRotate=6*ss;
 
-    //style
-    hour.style.transform=`rotate(${hRotate}deg)`
-    min.style.transform=`rotate(${mRotate}deg)`
-    sec.style.transform=`rotate(${sRotate}deg)`
+    // style-----------------------------------
+    hour.style.transform=`rotate(${hRotate}deg)`;
+    min.style.transform=`rotate(${mRotate}deg)`;
+    sec.style.transform=`rotate(${sRotate}deg)`;
+
+    // AM - PM format
+    ampm = hh >= 12 ? 'PM' : 'AM';
+    hh = hh % 12;  
+
+    // -----------------------------------------
+    timeEl.innerHTML = `${hh < 10 ? `0${hh}` : hh} : ${mm < 10 ? `0${mm}` : mm}  ${ampm}`;
+    dateEl.innerHTML = `${days[day]}, ${months[month]} ${date}`;
+
+    var times = setTimeout(function(){ startTime() }, 500);
 }
 
-setInterval(setDial);
+setDial();
+
+setInterval(setDial, 1000);
